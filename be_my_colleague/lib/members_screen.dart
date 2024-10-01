569f5ae3332b1b86e054b817d52e246d3afc4fcd
haelim_lee <86578107+Haelim-lee@users.noreply.github.com>
2024-10-01 14:55:16 +0900

    전화걸기 생성 및 아이콘추가 (#3)

--- be_my_colleague/lib/members_screen.dart
+++ be_my_colleague/lib/members_screen.dart
@@ -1,5 +1,6 @@
 import 'package:be_my_colleague/model/club.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MembersScreen extends StatefulWidget {
   final Club club;
@@ -19,7 +20,26 @@ class MembersScreenState extends State<MembersScreen> {
         .sort((a, b) => b.permission.index.compareTo(a.permission.index));
 
     return Scaffold(
-      appBar: AppBar(title: Text('회원목록')),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(
+              Icons.supervised_user_circle_sharp,
+              size: 30, // 아이콘 크기 조정
+              color: Colors.black, // 아이콘 색상
+            ),
+            SizedBox(width: 8), // 아이콘과 텍스트 사이의 간격
+            Text(
+              '회원정보',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: ListView.builder(
         itemCount: club.members.length,
         itemBuilder: (context, index) {
@@ -27,11 +47,25 @@ class MembersScreenState extends State<MembersScreen> {
           return ListTile(
             leading: Icon(Icons.account_circle),
             title: Text(member.name),
-            subtitle: Text(
-                'Permission: ${member.permission.toString().split('.').last}'),
+            subtitle:
+                Text('등급: ${member.permission.toString().split('.').last}'),
+            trailing: IconButton(
+              icon: Icon(Icons.phone),
+              onPressed: () => _makePhoneCall(member.phoneNumber),
+            ),
           );
         },
       ),
     );
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    var url = Uri.parse('tel:${phoneNumber}');
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
