189b1b0af05a8b7192901b5e0c9504c270fe9aa0
unknown <jaywapp16@gmail.com>
2024-11-05 22:55:18 +0900

    일정 추가 (권한에 따른) 기능 추가

--- be_my_colleague/lib/model/schedule.dart
+++ be_my_colleague/lib/model/schedule.dart
@@ -1,5 +1,6 @@
 import 'package:be_my_colleague/model/member.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Schedule {
   String id = '';
@@ -14,23 +15,60 @@ class Schedule {
       this.content, this.participantMails);
 
   Schedule.FromRow(List<Object?> row) {
-    this.id = (row.elementAtOrNull(0) as String) ?? '';
-    var dateStr = (row.elementAtOrNull(1) as String) ?? '';
-    this.name = (row.elementAtOrNull(2) as String) ?? '';
-    this.description = (row.elementAtOrNull(3) as String) ?? '';
-    this.location = (row.elementAtOrNull(4) as String) ?? '';
-    this.content = (row.elementAtOrNull(5) as String) ?? '';
-    var membersStr = (row.elementAtOrNull(6) as String) ?? '';
+    this.id = ConvertToString(row.elementAtOrNull(0));
+    var dateStr = ConvertToString(row.elementAtOrNull(1));
+    this.name = ConvertToString(row.elementAtOrNull(2));
+    this.description = ConvertToString(row.elementAtOrNull(3));
+    this.location = ConvertToString(row.elementAtOrNull(4));
+    this.content = ConvertToString(row.elementAtOrNull(5));
+    var membersStr = ConvertToString(row.elementAtOrNull(6));
 
     this.dateTime = DateTime.parse(dateStr ?? '');
-    this.participantMails = membersStr.split(',').map((o) => o.trim()).toList();
+
+    if(membersStr.length > 0)
+      this.participantMails = membersStr.split(',').map((o) => o.trim()).toList();
+  }
+
+  String ConvertToString(Object? obj) {
+    if (obj == null) return '';
+
+    try {
+      return obj as String;
+    } catch (e) {
+      return '';
+    }
+  }
+
+  List<Object> ToRow() {
+    List<Object> row = [];
+    row.add(id);
+    row.add(DateFormat('yyyy-MM-dd').format(dateTime));
+    row.add(name);
+    row.add(description);
+    row.add(location);
+    row.add(content);
+
+    var result = '';
+
+    var targets = participantMails.toList();
+
+    for (int i = 0; i < targets.length; i++) {
+      result += targets[i];
+      if (i != targets.length - 1) {
+        result += ', ';
+      }
+    }
+
+    row.add(result);
+
+    return row;
   }
 
   List<Object> ToRowWhenRemove(String remove) {
     List<Object> row = [];
 
     row.add(id);
-    row.add('${dateTime.year}-${dateTime.month}-${dateTime.day}');
+    row.add(DateFormat('yyyy-MM-dd').format(dateTime));
     row.add(name);
     row.add(description);
     row.add(location);
--- be_my_colleague/lib/schedule_screen.dart
+++ be_my_colleague/lib/schedule_screen.dart
@@ -1,14 +1,14 @@
+import 'dart:ffi';
+
 import 'package:be_my_colleague/Styles.dart';
 import 'package:be_my_colleague/data/data_center.dart';
-import 'package:be_my_colleague/model/account.dart';
-import 'package:be_my_colleague/model/club.dart';
 import 'package:be_my_colleague/model/schedule.dart';
+import 'package:be_my_colleague/schdule_add.dart';
 import 'package:be_my_colleague/schedule_block.dart';
 import 'package:be_my_colleague/schedule_detail.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleScreen extends StatefulWidget {
-
   final DataCenter dataCenter;
   final String clubID;
 
@@ -19,7 +19,6 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class ScheduleScreenState extends State<ScheduleScreen> {
-  
   String convert(DateTime now) {
     return "${now.year.toString()}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')} ${now.hour.toString().padLeft(2, '0')}-${now.minute.toString().padLeft(2, '0')}";
   }
@@ -31,41 +30,60 @@ class ScheduleScreenState extends State<ScheduleScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Styles.CreateHeader(Icons.calendar_month, '일정정보'),
-      ),
-
-      body: FutureBuilder(
-        future: widget.dataCenter.GetSchedules(widget.clubID), 
-        builder: (context, snapshot){
-          return ListView.builder(
-            itemCount: snapshot?.data?.length ?? 0,
-            itemBuilder: (BuildContext ctx, int index) {
-              var schedule = snapshot?.data?.elementAtOrNull(index);
-              return CreateBlock(schedule);
-            },
-          );
-        })
-      );
-  }
-
-  Widget CreateBlock(Schedule? schedule){
-    return ScheduleBlock(
-              onTap: () => {
-                    Navigator.push(
+        appBar: AppBar(
+          title: Styles.CreateHeader(Icons.calendar_month, '일정정보'),
+        ),
+        body: FutureBuilder(
+            future: widget.dataCenter.GetSchedules(widget.clubID),
+            builder: (context, snapshot) {
+              return ListView.builder(
+                itemCount: snapshot?.data?.length ?? 0,
+                itemBuilder: (BuildContext ctx, int index) {
+                  var schedule = snapshot?.data?.elementAtOrNull(index);
+                  return CreateBlock(schedule);
+                },
+              );
+            }),
+        floatingActionButton: FutureBuilder(
+            future: widget.dataCenter.IsAdmin(widget.clubID),
+            builder: (context, snapshot) {
+              if (snapshot?.data ?? false) {
+                return FloatingActionButton(
+                  onPressed: () async {
+                    var result = await Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ScheduleDetail(
-                          dataCenter: widget.dataCenter,
-                          clubID: widget.clubID,
-                          scheduleID: schedule?.id ?? '',
-                        ),
+                        builder: (context) => new ScheduleAddBlock(
+                            clubID: widget.clubID,
+                            dataCenter: widget.dataCenter),
                       ),
-                    ),
-                  },
-              schedule: schedule);
+                    );
 
+                    setState(() {});
+                  },
+                  child: Icon(Icons.add),
+                );
+              } else {
+                return SizedBox.shrink();
+              }
+            }));
+  }
+
+  Widget CreateBlock(Schedule? schedule) {
+    return ScheduleBlock(
+        onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ScheduleDetail(
+                    dataCenter: widget.dataCenter,
+                    clubID: widget.clubID,
+                    scheduleID: schedule?.id ?? '',
+                  ),
+                ),
+              ),
+            },
+        schedule: schedule);
   }
 }
